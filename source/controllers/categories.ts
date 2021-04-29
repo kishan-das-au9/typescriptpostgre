1e5bc0c3109a7aab4b6d10b2e0e172a40ac152d1
yogesh.ms <kyogesh.ms@gmail.com>
2021-04-29 16:37:24 +0530

    Category update and delte query

--- source/controllers/categories.ts
+++ source/controllers/categories.ts
@@ -39,11 +39,11 @@ const addCategory = async (req: Request, res: Response, next: NextFunction) => {
 
     // Adding cby manually for now.
     // Todo: Later get the data from sessions
-    const cby = 1
+    const userid = 1
 
     let query = `INSERT INTO categories
     (catname, ct, cby)
-    VALUES ('${dataObj.catname}','${moment.utc().format('YYYY-MM-DD HH:mm:ss')}', ${cby})`;
+    VALUES ('${dataObj.catname}','${moment.utc().format('YYYY-MM-DD HH:mm:ss')}', ${userid})`;
 
     await client.query(query);
 
@@ -55,5 +55,63 @@ const addCategory = async (req: Request, res: Response, next: NextFunction) => {
 
 };
 
+const updateCategory = async (req: Request, res: Response, next: NextFunction) => {
+  try {
 
-export default { getAllCategories, addCategory };
+    const joiSchema = Joi.object().keys({
+      catid: Joi.number().integer().required(),
+      catname: Joi.string().required().min(2).max(100).trim(),
+    });
+
+    const { error: joiError, value: dataObj } = joiSchema.validate(req.body);
+
+    if (joiError) {
+      return res.json({ errors: formatJoiValErrors(joiError), success: false, msg: 'Check Parameters' });
+    }
+
+    // Adding cby manually for now.
+    // Todo: Later get the data from sessions
+    const userid = 1
+
+    let query = `UPDATE categories
+    SET catname = '${dataObj.catname}', mt = '${moment.utc().format('YYYY-MM-DD HH:mm:ss')}', mby = ${userid}
+    WHERE id = ${dataObj.catid}`;
+
+    await client.query(query);
+
+    return res.json({ success: true, msg: "Category Updated" });
+
+  } catch (error) {
+    return res.json({ success: false, message: error });
+  }
+
+};
+
+const deleteCategory = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+
+    const joiSchema = Joi.object().keys({
+      catid: Joi.number().integer().required(),
+    });
+
+    const { error: joiError, value: dataObj } = joiSchema.validate(req.body);
+
+    if (joiError) {
+      return res.json({ errors: formatJoiValErrors(joiError), success: false, msg: 'Check Parameters' });
+    }
+
+    let query = `DELETE FROM categories
+    WHERE id = ${dataObj.catid}`;
+
+    await client.query(query);
+
+    return res.json({ success: true, msg: "Category Deleted" });
+
+  } catch (error) {
+    return res.json({ success: false, message: error });
+  }
+
+};
+
+
+export default { getAllCategories, addCategory, updateCategory, deleteCategory };
--- source/routes/categories.ts
+++ source/routes/categories.ts
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.post('/categorylist', category.getAllCategories);
 router.post('/addcategory', category.addCategory);
+router.post('/updatecategory', category.updateCategory);
+router.post('/deletecategory', category.deleteCategory);
 
 export = router;
